f64fca0724bdb5be5f6c4b453f9b39f40122072d
peterxmr <peter.kozuch@zoho.com>
2026-01-20 16:05:14 +0100

    Fix vulnerable dependencies with updating springframework.

--- src/main/java/com/shipmonk/testingday/controller/ExchangeRatesController.java
+++ src/main/java/com/shipmonk/testingday/controller/ExchangeRatesController.java
@@ -2,13 +2,14 @@ package com.shipmonk.testingday.controller;
 
 import com.shipmonk.testingday.api.ExchangeRate;
 import com.shipmonk.testingday.service.ExchangeRateService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,8 +35,8 @@ public class ExchangeRatesController {
      * @param day date in ISO format (YYYY-MM-DD)
      * @return ExchangeRate response with EUR as base currency and rates for other currencies
      */
-    @RequestMapping(method = RequestMethod.GET, path = "/{day}")
-    public ResponseEntity<ExchangeRate> getRates(@PathVariable("day") String day) {
+    @GetMapping("/{day}")
+    public ResponseEntity<ExchangeRate> getRates(@PathVariable String day) {
         logger.info("ExchangeRatesController: Received request for exchange rates on date: {}", day);
 
         ExchangeRate exchangeRate = exchangeRateService.getExchangeRates(day);
